faec3c73c223b5f05f1412be4faf0fc5fcfb0131
Ryan Wood <r0178658@gmail.com>
2017-05-02 16:28:25 -0400

    Working on some different options for the flipcards to avoid overlap

--- b-site/scripts/scripts2.js
+++ b-site/scripts/scripts2.js
@@ -6,3 +6,28 @@ $(document).on('click', '.navbar-collapse.in', function(e){
            } 
         });
 
+
+//$('.flip').mouseenter(function(){
+//    $(this).find('.card').addClass('flipped').mouseleave(function(){
+//        $(this).removeClass('flipped');
+//    });
+//    return false;
+//})
+
+
+$('.card').mouseenter(function(){
+    $(this).find('.front').toggleClass('hider').fadeOut(1000);
+    $(this).find('.back').toggleClass('hider').fadeIn(900);
+});
+
+$('.card').mouseleave(function(){
+    $(this).find('.front').toggleClass('hider').fadeIn(900);
+    $(this).find('.back').toggleClass('hider').fadeOut(1000);
+});
+
+
+
+
+
+
+
